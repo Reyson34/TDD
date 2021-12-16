a10b78dcf15c98a3df3a8c9f5e4c594b438611fd
aram <aram@goot.pro>
2021-12-16 12:57:24 +0100

    Adds formatter

---
+++ typescript-jest/src/Formatter.test.ts
@@ -0,0 +1,18 @@
+import { Formatter } from "./Formatter";
+
+describe('Formatter', () => {
+    it('Formats array to formatted string', () => {
+        let formatter = new Formatter();
+
+        const result = formatter.format([
+            ['Date', '2021-12-02'],
+            ['Amount', '+500'],
+            ['Balance', '500'],
+        ])
+
+        expect(result).toBe(
+    'Date       Amount  Balance\n' +
+            '2021-12-02   +500      500\n'
+        )
+    })
+})
