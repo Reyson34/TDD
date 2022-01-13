c67b97b284b08827153cc9a18cce640c045d35b1
romainCivel <romain@goot.pro>
2022-01-13 15:09:52 +0100

    Ajout d'une methode de calcul de la taille des headers

--- typescript-jest/src/Formatter.test.ts
+++ typescript-jest/src/Formatter.test.ts
@@ -1,18 +1,52 @@
 import { Formatter } from "./Formatter";
 
+
+
+
 describe('Formatter', () => {
-    it('Formats array to formatted string', () => {
+    it('Max length of each Column with padding', () => {
         let formatter = new Formatter();
 
-        const result = formatter.format([
+        const data = [
             ['Date', '2021-12-02'],
             ['Amount', '+500'],
             ['Balance', '500'],
-        ])
+        ]
 
-        expect(result).toBe(
-            'Date        Amount  Balance\n' +
-            '2021-12-02   +500      500\n'
-        )
+        const expectedResult = [ 10, 8, 9 ]
+        const result = formatter.columnLength(data)
+
+        expect(result).toEqual(expectedResult);
+    })
+
+    it('Display the header of the result', () => {
+        let formatter = new Formatter();
+
+        const data = [
+            ['Date', '2021-12-02'],
+            ['Amount', '+500'],
+            ['Balance', '500'],
+        ]
+
+        const expectedResult = "Date        Amount  Balance"
+        const result = formatter.format(data)
+
+        expect(result).toBe(expectedResult)
+    })
+
+    it('Formats array to formatted string', () => {
+        let formatter = new Formatter();
+
+        const data = [
+            ['Date', '2021-12-02'],
+            ['Amount', '+500'],
+            ['Balance', '500'],
+        ]
+        const expectedResult = 
+                'Date        Amount  Balance\n' +
+                '2021-12-02   +500      500 \n'
+        const result = formatter.format(data)
+
+        expect(result).toBe(expectedResult)
     })
 })
--- typescript-jest/src/Formatter.ts
+++ typescript-jest/src/Formatter.ts
@@ -7,8 +7,72 @@ export class Formatter {
 
     // 'Date       Amount  Balance\n' +
     // '2021-12-02   +500      500\n'
+    /*
+        Premiere colonne des dates: aligné à gauche
+        les autres colonne aligné à droite moins 1 caractere
+
+        Date    __Amount__Balance\n
+        XXXXXXXX__ YYYY___   ZZZ_\n
+
+    */
+    /*
+        pour chaque tableau de données
+            chercher la plus longue valeur
+            pour toutes les colonnes sauf la premiere lui ajouter 2
+
+            
+
+        pour chaque tableau de données
+            prendre le premier et generer la ligne de titre
+                ()
+            Pour chaque autre données
+                premiere donnée: aligner à gauche
+                sinon aligner à droite -1 caractere
+
+
+
+
+    */
+
+    columnLength = (arr) => {
+        // contient la longeur max de chaque colonne
+        const maxLenght = []
+        arr.forEach((col, indexCol) => {
+            col.forEach((row, indexRow) => {
+                if(row.length > (maxLenght[indexCol] || 0))
+                    maxLenght[indexCol] = row.length
+            });
+            if(indexCol > 0) maxLenght[indexCol] += 2
+        });
+        return maxLenght
+    }
 
     format = (arr) => {
+        //les longueurs de chaque colonne
+        const colLength = this.columnLength(arr)
+
+        const result = []
+        //on va parcourir uniquement la premiere valeur de chaque ligne de arr
+        //ces valeurs sont les headers
+        arr.forEach((col, indexCol) => {
+            col.forEach((row, indexRow) => {
+                if(row == 0){ // cas des headers
+                    if(indexCol == 0) result[0]= row.padEnd(colLength[0]) 
+                    else result[indexCol] = row.padEnd(row.length).padStart(colLength[indexCol])//    (colLength - col.lenght-1) row.length 1
+                }else{
+
+                }
+            });
+
+        });
+
+        return result.join('')
+    }
+
+
+
+
+    /*format = (arr) => {
         const res = []
 
         arr.forEach((row, index) => {
@@ -32,6 +96,6 @@ export class Formatter {
         }
 
         return result 
-    }
+    }*/
 }
 
