f4164b7022948b7ba42bd207c24834300e666afc
Félix Cottet <felix.cottet@in2car.com>
2022-01-21 14:49:46 +0100

    Add formating deposit

--- typescript-jest/src/Account.test.ts
+++ typescript-jest/src/Account.test.ts
@@ -1,22 +1,17 @@
 import { Account } from './Account'
 import { Balance } from './Balance';
+import { Formatter } from './Formatter';
 
 describe('Account', () => {
-  it('my new account prints date, amount and balance', () => {
-    const account = new Account(new Balance())
-    let result = account.printStatement();
-
-    expect(result).toBe('Date\tAmount\tBalance')
-  })
-
+  
   it('my new account can deposit and returns my date, amount and balance', () => {
-    const account = new Account(new Balance())
+    const account = new Account(new Balance(), new Formatter())
     account.deposit(500);
     let result = account.printStatement();
 
     let expectedResult = 
       'Date        Amount  Balance\n' +
-      '2021-12-02   +500      500 \n'
+      '2022-01-21   +500      500 \n'
     expect(result).toBe(expectedResult)
   })
 })
--- typescript-jest/src/Account.ts
+++ typescript-jest/src/Account.ts
@@ -1,21 +1,42 @@
-import {Balance} from "./Balance";
+import { Balance } from "./Balance";
+import { Formatter } from "./Formatter";
+import { Transaction } from "./Transaction"
 
 export class Account {
   balance: Balance;
+  formatter: Formatter;
+  list: Array<Transaction>;
 
-  constructor(balance: Balance) {
+  constructor(balance: Balance, formatter: Formatter) {
     this.balance = balance;
+    this.formatter =  formatter;
+    this.list = new Array
+  }
+
+  formatTab = (list) => {
+    const initialTab = [
+      ['Date'],
+      ['Amount'],
+      ['Balance']
+    ]
+    for (var i = 0; i < list.length; i++) {
+      initialTab[0].push(list[i].date.toISOString().split('T')[0])
+      initialTab[1].push('+' + list[i].value)
+      initialTab[2].push(this.balance.calculate(list.slice(0, i + 1)).toString())
+    }
+    return initialTab
   }
 
   printStatement(): string {
     // needs to be in separated class
-    return 'Date\tAmount\tBalance';
+
+    const tranformTab = this.formatTab(this.list)
+
+    return this.formatter.format(tranformTab)
   }
 
   deposit(amount): void {
-    const balance = new Balance;
-    // balance.deposit(amount);
+    this.list.push(new Transaction(new Date(), amount))
   }
-
 
 }
